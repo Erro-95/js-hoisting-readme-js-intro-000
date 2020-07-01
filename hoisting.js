ee2bcc5c0e320a82338bf9f96b7b03e7aa5dad98
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-01 22:01:37 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,30 @@
-function callMe() {
-  var lyric;
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  lyric = "maybe";
-  return lyric;
-}
+// function callMe() {
+//   var lyric;
+//   console.log("I just met you...");
+//   console.log("and this is crazy..");
+//   console.log("but here's my number..");
+//   console.log("so call me");
+//   lyric = "maybe";
+//   return lyric;
+// }
 
 
-function crazy() {
-  // fix the code in here:
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
-  thisIsCrazy();
-}
+// function crazy() {
+//   // fix the code in here:
+//   var thisIsCrazy = function (){
+//     console.log("hey!!!")
+//   }
+//   thisIsCrazy();
+// }
 
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+// function sayMyName() {
+//   // fix the code in here:
+//   var name = "Cricky";
 
-  sayMy();
+//   sayMy();
 
-  function sayMy() {
-    var name = "Kristin";
-    console.log(name);
-  }
-}
+//   function sayMy() {
+//     var name = "Kristin";
+//     console.log(name);
+//   }
+// }
