25aeb6af6a9e04627dcffa8e9b87da5c63134b2a
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-01 22:06:27 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,18 +1,18 @@
 
 function callMe() {
-  var lyric
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  lyric = 'maybe'
   return lyric;
 }
-function crazy() {}
+function crazy() {
 //fix the code in here:
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
+}
 }
 function sayMyName() {
   // fix the code in here:
